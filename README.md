fda295035223c72b8ec93397fe5a243c11a1d7f5
Rodrigo Rolim Mendes de Alencar <alencar.fmce@imbel.gov.br>
2018-05-25 11:47:55 -0300

    fixed install script and added readme

---
+++ README.md
@@ -0,0 +1,13 @@
+# JPEO JTRS Standard APIs REDHAWK IDL Projects
+
+## Description
+
+Those Projects are based on JTNC Standard API Documents found in [www.public.navy.mil](http://www.public.navy.mil/jtnc/Pages/resources.aspx?filter=cat-api), minor changes are made in order to adapt the IDLs to the REDHAWK Target SDR IDL Repository.
+
+## Installation Instructions
+
+You just need to to run:
+
+	$ ./instal.sh 
+
+at the top level directory. It will generate the required source files, build and install the Interfaces libraries into $OSSIEHOME. Note: sudo privileges are required
--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ JTRS
 Packet
 DevMsgCtl
 Audio
+Vocoder
 DeviceIo
 SerialPort
 DevIOC
@@ -20,21 +21,37 @@ EOM
 
 echo "$MODULES" | while read -r MODULE
 do
-	echo "#############################################"
-	echo -e "#   \033[1mBuild and Installing $MODULE Module\033[0m"
-	echo "#############################################"
-	cd $MODULE/ 
-	echo -e "\033[1mConfigure...\033[0m"
-	./reconf
-	./configure
-	echo "\033[1mBuild and Install...\033[0m"
-	if ! sudo make install; then
-		echo "Failed to install $MODULE Module"
-		exit 1
-	fi
-	
-	wait
-	cd ..
+	if [ -e "$OSSIEHOME/lib/${MODULE^^}Interfaces.jar" ]; then
+		echo -e "\033[1mModule $MODULE already installed\033[0m"
+	else
+		echo "#############################################"
+		echo -e "#   \033[1mBuild and Installing $MODULE Module\033[0m"
+		echo "#############################################"
+		cd $MODULE/ 
 
+		echo -e "\033[1mConfigure...\033[0m"
+		if ! ./reconf; then
+			./reconf #try again
+			wait
+		fi
+		if ! ./configure; then
+			echo "Failed to configure $MODULE Module"
+			exit 1
+		fi
+
+		#echo -e "\033[1mBuilding...\033[0m"
+		#if ! make; then
+		#	echo "Failed to build $MODULE Module"
+		#	exit 1
+		#fi
+
+		echo -e "\033[1mBuilding and Installing...\033[0m"
+		if ! sudo make install; then
+			echo "Failed to install $MODULE Module"
+			exit 1
+		fi
+	
+		cd ..
+	fi	
 done
 
